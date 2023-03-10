933c7bd56a3491cd3e109d386e9375afa25eb0b3
Vahid MV <VahidMV30@gmail.com>
2023-03-10 20:00:12 +0330

    WebApp project created. Tailwind css & IranSans font & FontAwesome icons added.

---
+++ ClothingShop.WebApp/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=dashboard}/{action=index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=home}/{action=index}/{id?}");
+
+app.Run();
---
+++ ClothingShop.WebApp/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Pages/**/*.cshtml",
+    "./Views/**/*.cshtml",
+    "./Areas/Admin/Pages/**/*.cshtml",
+    "./Areas/Admin/Views/**/*.cshtml",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
